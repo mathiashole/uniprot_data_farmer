f87fc2276f2d4248bd3144b9840f44921f63137e
mathiashole <joacomangino@gmail.com>
2023-08-15 13:26:14 -0300

    need debuger

--- download_id.sh
+++ download_id.sh
@@ -47,8 +47,8 @@ getfile_data(){
       # Check if the line contains a valid ID
       if [[ $id =~ ^[A-Za-z0-9]+$ ]]; then
         # Download the FASTA file for each valid ID
-        url="https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=${id}&query=accession%${extension}"
-        file_name="uniprot_${id}${extension}"
+        url="https://rest.uniprot.org/uniprotkb/stream?format=${extension}&query=accession%${id}"
+        file_name="uniprot_${id}.${extension}"
         output_path="fileDownload/$file_name"
         curl -o "$output_path" "$url"
         echo "The file $file_name has been downloaded successfully."
